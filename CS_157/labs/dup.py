d29784a61071642d54d896e5b8e3c2c434ff6880
Hayes Crowley <hayes.crowley@locatorx.com>
2021-02-18 12:38:33 -0500

    created QA files for labs

--- CS_157/labs/dup.py
+++ CS_157/labs/dup.py
@@ -1,11 +1,26 @@
 
+# import os
+# import subprocess
+# for root, dirs, files in os.walk("/Users/frogLegg/Desktop/CS/CS_157/labs"):
+#     for file in files:
+# if file.endswith(".rtf"):
+#     print(file)
+#     fileString = str(file).replace(".rtf", "_QA.rtf")
+
+#             subprocess.run("mv ${file} ${fileString}")
+#             # print(fileString)
+#             # os.system("mv ${file} ${fileString}")
+#             # print(os.path.join(root, file))
+#             # os.system('mv')
+
 import os
-for root, dirs, files in os.walk("/Users/frogLegg/Desktop/CS/CS_157/labs"):
-    for file in files:
-        if file.endswith(".rtf"):
-            # print(file)
-            fileString = str(file).replace(".rtf", "_QA.rtf")
+import shutil
+
+for root, subdirs, files in os.walk("."):
+    for f in files:
+        if f.endswith(".rtf"):
+            fileString = str(f).replace(".rtf", "_QA.rtf")
+            print(f)
             print(fileString)
-            # os.system("mv ${file} ${fileString}")
-            # print(os.path.join(root, file))
-            # os.system('mv')
+            shutil.copy(os.path.join(root, f), os.path.join(root, fileString))
+            # os.(os.path.join(root, f), os.path.join(root, fileString))
