9889ba19f7ef672cb2941ae413a96f6a26c55d24
Hayes Crowley <hayes.crowley@locatorx.com>
2021-03-01 18:19:46 -0500

    finished HW

--- CS_157/labs/lab_06/lab_06.py
+++ CS_157/labs/lab_06/lab_06.py
@@ -1,14 +1,37 @@
-from trans import bank_list
+class Person:
 
-my_list = bank_list()
-total = 0.0
+    def __init__(self, first, last):
+        self.firstname = first
+        self.lastname = last
 
-my_list.append({"2020-05-13": -1000})
+    def Name(self):
+        return self.firstname + " " + self.lastname
 
-print("\nRichard Hayes Crowley CSC_157_lab_06\n")
-for item in my_list:
-    for key, value in item.items():
-        print(f"Date: {key} Bal: ${round(value,2):.2f}")
 
-        total += value
-print(f"\nTotal balance: ${round(total,2):.2f}")
+class Employee(Person):
+
+    def __init__(self, first, last, staffnum):
+        Person.__init__(self, first, last)
+        self.staffnumber = staffnum
+
+    def GetEmployee(self):
+        return self.Name() + ", " + str(self.staffnumber)
+
+
+x = Person("Sammy", "Student")
+y = Employee("Penny", "Peters", "805")
+
+
+class PartTime(Employee):
+
+    def __init__(self, first, last, staffnum, hourlyRate):
+        Employee.__init__(self, first, last, staffnum)
+        self.hourlyRate = hourlyRate
+
+    def GetEmployee(self):
+        return f"{Employee.GetEmployee(self)}, is a part time employee who makes: {str(self.hourlyRate)}"
+        # Modify the above code by deriving a new class from class Employee named as class 	PartTime and derive some objects based on this new class.
+
+
+partTimeEmployee = PartTime("Hayes", "Crowley", 123, 18.00)
+partTimeEmployee.GetEmployee()
