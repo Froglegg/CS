e3a90d6f936a73f7050e96a3e4e47cce3e854cd4
froglegg <hayescrowley@gmail.com>
2022-06-18 17:15:02 -0400

    C hello world

---
+++ Effective_C/chapters/01/hello_world.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void){
+    // if (puts("hello, world!") == EOF){
+    //     return EXIT_FAILURE;
+    // }
+    // return EXIT_SUCCESS;
+    printf("%s\n", "Hello, world!");
+    return EXIT_SUCCESS;
+}
